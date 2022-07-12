caeeb7624262dfca852742d9caf220549a0932f5
Anubhav Chauhan <63549878+Anubhav-Chauhan3367@users.noreply.github.com>
2022-07-13 02:59:16 +0530

    Added solution - LeetHub

--- 15. Diameter of a Binary Tree - GFG/15.-diameter-of-a-binary-tree.java
+++ 15. Diameter of a Binary Tree - GFG/15.-diameter-of-a-binary-tree.java
@@ -118,22 +118,24 @@ class Solution {
     // Function to return the diameter of a Binary Tree.
     int diameter(Node root) {
         // Your code here
-        if(root == null) return 0;
         
-        int d1 = height(root.left)+height(root.right)+1;
-        int d2 = diameter(root.left);
-        int d3 = diameter(root.right);
+        height(root);
+        return res;
+        
+    }    
+    int res = 0;
+    int height(Node root) {
+        // Your code here
+        if(root == null) return 0;
+      
+        int d1 = height(root.left);
+        
+        int d2 = height(root.right);
+      
+        int d3 = d1+d2+1;
        
-        
-        return Math.max(d1, Math.max(d2, d3));
-        
-    }
-    
-    int height(Node root){
-        
-        if(root == null) return 0;
-        
-        return Math.max(height(root.left), height(root.right))+1;
+        res = Math.max(res, d3);
+        return Math.max(d2, d1)+1;
         
     }
 }
