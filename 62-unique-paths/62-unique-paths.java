eaf2b6652ba4c3c8f2d7ad62e4192cfe6cc7d075
Anubhav Chauhan <63549878+Anubhav-Chauhan3367@users.noreply.github.com>
2022-07-31 17:42:48 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (40.65%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int[][] dp = new int[m][n];
-        
-        for(int i=0; i<m; i++){
-            dp[i][0] = 1;
+        int N = m+n-2;
+        int r = n-1;
+        double ans = 1;
+        for(int i=r+1; i<=N; i++){
+            ans *= i;
+            ans /= i-r;
         }
-        for(int i=0; i<n; i++){
-            dp[0][i] = 1;
-        }
-        
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        
-        return dp[m-1][n-1];
+        return (int)ans;
     }
 }
