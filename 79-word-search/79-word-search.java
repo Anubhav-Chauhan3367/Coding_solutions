31a514b2eb028ba86252cdcd6264a11af53bd2ee
Anubhav Chauhan <63549878+Anubhav-Chauhan3367@users.noreply.github.com>
2022-08-23 19:55:23 +0530

    Time: 180 ms (50.71%), Space: 42.6 MB (31.86%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -16,7 +16,7 @@ class Solution {
         
         if(index == word.length()) return true;
         
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] == '#') return false;
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] == '#' || board[i][j] != word.charAt(index)) return false;
         
         boolean res = false;
         
