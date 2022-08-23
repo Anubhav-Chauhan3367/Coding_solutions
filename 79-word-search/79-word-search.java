6184562efcbc595679f6dc51ed56b3b25282dc19
Anubhav Chauhan <63549878+Anubhav-Chauhan3367@users.noreply.github.com>
2022-08-23 19:48:38 +0530

    Time: 145 ms (71.10%), Space: 42.3 MB (50.56%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(wordSearch(board, word, i, j, 0)) return true;
+            }
+        }
+                
+        return false;
+    }
+    
+    public boolean wordSearch(char[][] board, String word, int i, int j, int index){
+        
+        if(index == word.length()) return true;
+        
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] == '#') return false;
+        
+        boolean res = false;
+        
+        if(board[i][j] == word.charAt(index)){
+            
+            char backTracker = board[i][j];
+            board[i][j] = '#';
+            res |= wordSearch(board, word, i+1, j, index+1);
+            res |= wordSearch(board, word, i-1, j, index+1);
+            res |= wordSearch(board, word, i, j+1, index+1);
+            res |= wordSearch(board, word, i, j-1, index+1);
+            board[i][j] = backTracker;
+            
+        }
+        
+        return res;
+        
+    }
+}
