86a9db22e7556a87fac5e46a5f7d9239d62c74f9
Anubhav Chauhan <63549878+Anubhav-Chauhan3367@users.noreply.github.com>
2022-08-24 18:52:48 +0530

    Time: 11 ms (37.20%), Space: 43.9 MB (22.95%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        
+        
+        List<String> ans = new ArrayList<String>();
+        
+        if(s.length() > 12){
+            return ans;    
+        }
+        
+        helper(s, ans, 0, "", 0);
+        
+        return ans;
+        
+    }
+    
+    public void helper(String s, List<String> ans, int indx, String curr, int dots){
+        
+        if(dots == 4 && indx == s.length()){
+            ans.add(curr.substring(0, curr.length()-1));
+            return;
+        }
+        if(dots > 4){
+            return;
+        }
+        
+        for(int i=indx; i<indx+3 && i<s.length(); i++){
+            
+            if(Integer.valueOf(s.substring(indx, i+1)) < 256 && (indx == i || s.charAt(indx) != '0')){
+                helper(s, ans, i+1, curr+s.substring(indx, i+1)+".", dots+1);   
+            }
+        }
+        
+    }
+    
+}
