e3dd7d6b97f65669da5918d72740817a00e68ecf
Anubhav Chauhan <63549878+Anubhav-Chauhan3367@users.noreply.github.com>
2022-09-02 20:38:56 +0530

    Time: 24 ms (12.20%), Space: 51 MB (83.12%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -2,24 +2,36 @@ class Solution {
     public int candy(int[] ratings) {
         
         int candies = 0;
-        int[] child = new int[ratings.length];
-        Arrays.fill(child, 1);
+        int up=0, down=0;
+        int prevSlope = 0;
         
         for(int i=1; i<ratings.length; i++){
-            if(ratings[i] > ratings[i-1]){
-                child[i] = child[i-1]+1;
+            
+            int currSlope = (ratings[i]>ratings[i-1])?1:((ratings[i]<ratings[i-1])?-1:0);
+            
+            if((prevSlope<0 && currSlope>=0) || (prevSlope>0 && currSlope==0)){
+                candies += (sum(up)+sum(down)+Math.max(up, down));
+                up = 0;
+                down = 0;
             }
+            
+            if(currSlope > 0) up++;
+            else if(currSlope < 0) down++;
+            else candies++;
+            
+            prevSlope = currSlope; 
+            
         }
-        for(int i=ratings.length-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                child[i] = Math.max(child[i], child[i+1]+1);
-            }
-        }
-        for(int a: child){
-            candies += a;
-        }
+        
+        candies += (sum(up)+sum(down)+Math.max(up, down)+1);
+        
+        
         
         return candies;
         
+    }
+    
+    private int sum(int a){
+        return (a*(a+1))/2;
     }
 }
